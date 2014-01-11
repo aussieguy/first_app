6bc230d384ecacc6e411c586009d9c1c08ce12c0
Glen Hobby <glen.hobby@visionaustralia.org>
2014-01-11 20:23:41 +1100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '641f18f6ffc79ad29bb5ffa2d27bfadf62445fbcae649ed6f978f3d5ef91b6d6c1e79ae9f4937b213c591663c6f3e91b28d8aa4b6b514bf3258ae1c322bc2712'
